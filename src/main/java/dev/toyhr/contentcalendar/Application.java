d60659c3e50503ec59d074d98a024eb60566c348
Oluwatoyin <myworldco@gmail.com>
2023-06-27 22:56:02 +0200

    Content Calendar CRUD

--- src/main/java/dev/toyhr/contentcalendar/Application.java
+++ src/main/java/dev/toyhr/contentcalendar/Application.java
@@ -2,12 +2,15 @@ package dev.toyhr.contentcalendar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
-public class ContentCalendarApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ContentCalendarApplication.class, args);
+		SpringApplication.run(Application.class, args);
+
+
 	}
 
 }
